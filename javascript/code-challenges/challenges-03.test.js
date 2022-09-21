d2fc1a7e499bda1d35ead7d472c48e8982c610e9
Cianenry Danan <cianenryd@gmail.com>
2022-09-21 14:31:32 -0700

    Finished.

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -106,7 +106,8 @@ const snorlaxData = {
 };
 
 const getBaseStatGreaterThan = (arr, minBaseStat) => {
-  // Solution code here...
+  const highStats = arr.filter( highStat => highStat.baseStat > minBaseStat);
+  return highStats;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -254,7 +255,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should return an array containing the stats that are greater than the input', () => {
     expect(getBaseStatGreaterThan(snorlaxData.stats, 75)).toStrictEqual([ { stat: { url: 'https://pokeapi.co/api/v2/stat/5/', name: 'special-defense' }, effort: 2, baseStat: 110 } ]);
     expect(getBaseStatGreaterThan(snorlaxData.stats, 75).length).toStrictEqual(1);
